b1605548ce7ed082a71db294719dbf7932378876
mirza2003-dotcom <mirzabayazidbeg@gmail.com>
2026-01-13 17:52:52 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
+  base: '/zomato_clone/',
   plugins: [react()],
   build: {
     outDir: 'dist',
     emptyOutDir: true
   }
 })
-
 
